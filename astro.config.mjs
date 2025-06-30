67ee712717b9484263110902009b90c11020ee4d
Amina Rafi <amnarafi43@gmail.com>
2025-07-01 01:40:45 +0500

    Initial commit from friend's template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thewasif.github.io",
+  site: "https://girlmeetsfast.github.io",
 });
